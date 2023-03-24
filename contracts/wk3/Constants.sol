ad563cff635641ca786454b59333b53fe3beac3f
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-03-24 22:37:36 +0800

    add Constants contract
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ contracts/wk3/Constants.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// Constants use less gas when been exscuted
+// execution cost	378 gas (Cost only applies when called by a contract)
+contract Constants {
+    address public constant MY_ADDRESS = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    uint public constant MY_UINT = 506;
+}
+
+// Variables use more gas when been executed
+// execution cost	2489 gas (Cost only applies when called by a contract)
+contract Var {
+    address public MY_ADDRESS = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
