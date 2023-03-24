30180eb6df39bc9bef93b2f5a610125aea440b0a
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-03-24 17:44:12 +0800

    add LocalVariables contract

---
+++ contracts/wk3/LocalVariables.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract LocalVariables {
+    uint public i;
+    bool public b;
+    address public myAddr;
+
+    function foo() external {
+        uint x = 123;
+        bool f = false;
+        // more code
+        x += 456;
+        f = true;
+        // after the function excuted, values of local variables will be gone.
+        i = 123;
+        b = true;
+        myAddr = address(1);
+    }
+}
