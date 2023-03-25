aabaca89d0104ff5f9d2b1b26a6d8bff49883235
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-03-26 07:31:30 +0800

    add FunctionOutputs contract
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ contracts/wk3/FunctionOutputs.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// Retrun multiple outputs
+// Named outputs
+// Destructuring Assignment
+
+contract FunctionOutputs {
+    // costs 479 gas
+    function returnMany() public pure returns (uint, bool) {
+        return (1, true);
+    }
+
+    // costs 457 gas
+    function named() public pure returns (uint x, bool b) {
+        return (1, true);
+    }
+
+    // costs 501 gas
+    function assigned() public pure returns (uint x, bool b) {
+        x = 1;
+        b = true;
+    }
+
+    function destructingAssignments() public pure {
+        // capturing outputs from a function
+        (uint x, bool b) = returnMany();
+        // ignore first output 
+        (, bool _b) = returnMany();
+        // ignore second output 
+        (uint _x, ) = returnMany();
+    }
+}
