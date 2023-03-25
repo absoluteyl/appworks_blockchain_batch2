952f1bef7ae36e3d21c4278864c42f2e81c6eb57
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-03-26 07:46:35 +0800

    add IfElse contract
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ contracts/wk3/IfElse.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract IfElse {
+    function exmaple(uint _x) external pure returns (uint) {
+        if (_x < 10) {
+            return 1;
+        } else if (_x < 20) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+    function ternary(uint _x) external pure returns (uint) {
+        // if (_x < 10) {
+        //     return 1;
+        // }
+        // return 2;
+        // 👇 becomes
+        return _x < 10 ? 1: 2;
+    }
+}
