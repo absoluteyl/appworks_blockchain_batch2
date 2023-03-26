f663c5535417fc3a731eacb393c30ccd5c1bca03
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-03-26 08:23:41 +0800

    add Event contract
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ contracts/wk3/Event.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// Event can stores data on the blockchain but not able to retrive
+contract Event {
+    event Log(string message, uint val);
+    event IndexedLog(address indexed sender, uint val);
+    // note: up to 3 index can be added in event
+
+    // Transactional function
+    function example() external {
+        emit Log("foo", 506);
+        emit IndexedLog(msg.sender, 579);
+    }
+
+    event Message(address indexed _from, address indexed _to, string message);
+    function sendMessage(address _to, string calldata message) external {
+        emit Message(msg.sender, _to, message);
+    }
+}
