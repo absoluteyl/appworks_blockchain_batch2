cdb8059e7c5aaf9e8a354ded31bcc1e3a22e76ad
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-03-28 10:23:34 +0800

    add wk4 folder and add monday course handson
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ contracts/wk4/DataStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract DataStorage {
+    address public owner;
+    uint public x;
+    string public str;
+    string public str2;
+
+    constructor(uint _x) {
+        owner = msg.sender;
+        x = _x;
+    }
+
+    function takeCallData(string calldata _string) external {
+        str = _string;
+    }
+
+    // if we call takeCallData first then takeMemory,
+    // when variable - str is reuse in takeMemory, it would cost less gas since the storage slot is already
+    // initiated in takeCallData. But if we use a new variable - str2, the gas cost of initiate storage slot
+    // by data in memory is higher then initiate storage slot by calldata because there's an additional step
+    // to copy calldata value into memory.
+
+    function takeMemory(string memory _string) external {
+        str = _string;
+        // str2 = _string;
+    }
+}
