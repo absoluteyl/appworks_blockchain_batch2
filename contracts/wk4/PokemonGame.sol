38be4a0aa8229951b5182b4149507c0d65c6f824
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-02 19:39:11 +0800

    update pokemon game contract to let use find their pokemon to enhance by pokemon name
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- contracts/wk4/PokemonGame.sol
+++ contracts/wk4/PokemonGame.sol
@@ -7,43 +7,89 @@ pragma solidity ^0.8.19;
 // 一個玩家可以有多個寶可夢
 // 需要有一個 create 的方法來創造出寶可夢
 // （optional) 可付費 0.1 ETH 來增加該玩家所擁有的特定寶可夢的攻擊或是防禦
+
+// library PokemonHelper {
+    // function find(
+    //     uint[] memory arr,
+    //     uint x
+    // ) internal view returns (uint) {
+    //     for(uint i = 0; i < arr.length; i++) {
+    //         if(arr[i] == x) {
+    //             return i;
+    //         }
+    //     }
+    //     revert("Not Found.");
+    // }
+
+//     function findByName(
+//         string[] storage arr,
+//         string str
+//     ) internal view returns (uint) {
+//         for(uint i = 0; i < arr.length; i++) {
+//             if(keccak256(abi.encodePacked(arr[i])) == keccak256(abi.encodePacked(str))) {
+//                 return i;
+//             }
+//         }
+//         revert("Not Found.");
+//     }
+// }
+
 contract PokemonGame {
     struct Pokemon {
-        string name;
-        uint attack;
-        uint defense;
+        uint   id;      // Unique ID amount all pokemons.
+        string name;    // Pokemon Name given by owner.
+        uint   attack;  // Attack Points.
+        uint   defense; // Defense Points.
     }
-    mapping (address => Pokemon[]) ownerToPokemons;
 
-    // Create Pokemon of Msg Sender
+    Pokemon[] public pokemons;
+    mapping (address => uint[]) public ownerToPokemonIds;
+
+    // Pokemon Helper
+    function findByName(string calldata _name) internal view returns (uint) {
+        for(uint i = 0; i < pokemons.length; i++) {
+            if(keccak256(abi.encodePacked(pokemons[i].name)) == keccak256(abi.encodePacked(_name))) {
+                return i;
+            }
+        }
+        revert("Not Found.");
+    }
+
+    // Create Pokemon for Msg Sender
     function create(string calldata _name) external {
         // Generate random attack, defense points.
         uint _attack = uint(keccak256(abi.encodePacked(_name))) % 1000 * 5;
         uint _defense = uint(keccak256(abi.encodePacked(_name))) % 100 * 60;
 
         // Init the pokemon
-        Pokemon memory _pokemon = Pokemon(_name, _attack, _defense);
+        uint _id = pokemons.length + 1;
+        Pokemon memory _pokemon = Pokemon(_id, _name, _attack, _defense);
 
         // Add it to sender's collection.
-        ownerToPokemons[msg.sender].push(_pokemon);
+        pokemons.push(_pokemon);
+        ownerToPokemonIds[msg.sender].push(_id);
     }
 
     // List all Pokemons of Msg Sender
-    function list() external view returns (Pokemon[] memory _pokemons) {
-        return ownerToPokemons[msg.sender];
+    function listMyPokemons() external view returns (uint[] memory) {
+        return ownerToPokemonIds[msg.sender];
     }
 
     // Add Attack points of selected pokemon of Msg Sender
-    function addAttack(uint pokemonIdx) external payable {
+    function addAttack(string calldata _name) external payable {
         require((msg.value == 0.01 ether), "Amount should be 0.01 Ether");
-        Pokemon storage _pokemon = ownerToPokemons[msg.sender][pokemonIdx];
+        
+        uint _idx = findByName(_name);
+        Pokemon storage _pokemon = pokemons[_idx];
         _pokemon.attack += 100;
     }
 
     // Add Defense points of selected pokemon of Msg Sender
-    function addDefense(uint pokemonIdx) external payable {
+    function addDefense(string calldata _name) external payable {
         require((msg.value == 0.01 ether), "Amount should be 0.01 Ether");
-        Pokemon storage _pokemon = ownerToPokemons[msg.sender][pokemonIdx];
+        
+        uint _idx = findByName(_name);
+        Pokemon storage _pokemon = pokemons[_idx];
         _pokemon.defense += 100;
     }
 }
