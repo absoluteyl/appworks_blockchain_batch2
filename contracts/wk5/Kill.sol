b3619674842297c622e26c210e543bf07653c05c
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-05 09:52:35 +0800

    add kill contract
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ contracts/wk5/Kill.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// ref: https://www.youtube.com/watch?v=ajCsPRl5S3Q
+
+// selfdestruct
+// - delete contract
+// - force send Ether to any address
+contract Kill {
+    constructor () payable {}
+
+    function kill() external {
+        selfdestruct(payable(msg.sender));
+    }
+
+    function test() external pure returns(uint) {
+        return 506;
+    }
+}
+
+contract Helper {
+    function getBalance() external view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function kill(Kill _kill) external {
+        _kill.kill();
+    }
+}
