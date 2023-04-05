e7919fa43e24f1c77839f9c95259cd20c0495a80
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-05 09:32:19 +0800

    add immutable contract
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ contracts/wk5/Immutable.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// ref: https://www.youtube.com/watch?v=nQi8lVi4xT4
+
+contract Immutable {
+    // make this variable as a constant, add immutable (will also save some gas)
+    // consumed 50116 gas, will consume 52569 gas if we don't use immutable.
+    address public immutable owner;
+    
+    constructor () {
+        owner = msg.sender;
+    }
+
+    uint public x;
+
+    function foo() external {
+        require(msg.sender == owner);
+        x += 1;
+    }
+}
