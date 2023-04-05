bc7b3c7b93e0ee718b9fe922c404dc576d5812a8
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-05 10:07:20 +0800

    add ownable contract
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ contracts/wk5/Ownable.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// ref: https://www.youtube.com/watch?v=QEJYSuyYOfw
+
+// state variables
+// global variables
+// function modifier
+// function
+// error handling
+
+contract Ownable {
+    address public owner;
+
+    constructor () {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Permission Denied.");
+        _;
+    }
+
+    function setOwner(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "Invalid Address.");
+        owner = _newOwner;
+    }
+
+    function onlyOwnerCanCall() external onlyOwner {}
+    function anyoneCanCall() external {}
+}
