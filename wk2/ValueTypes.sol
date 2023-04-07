f9fecbb0d40c261b57ef45150f8a7d3b022cd897
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-07 11:44:39 +0800

    lint wk2 files
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- wk2/ValueTypes.sol
+++ wk2/ValueTypes.sol
@@ -9,7 +9,7 @@ contract ValueTypes {
     bool public b = true;
     uint public u = 506; // uint = uint256 0 to (2**256 - 1)
                          //        uint8   0 to (2**8 - 1)
-                         //        uint16  0 to (2**16 - 1) 
+                         //        uint16  0 to (2**16 - 1)
     int public i = -506; // int  = int256  -2**255 to 2**255 -1
                          //        int128  -2**127 to 2**127 -1
     int public minInt = type(int).min;
