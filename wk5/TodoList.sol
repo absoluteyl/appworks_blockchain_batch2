7b3443aaf8abf0cc18575ddd28a14cfd1823ec58
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-08 21:24:03 +0800

    remove for loop to reduce gas cost
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- wk5/TodoList.sol
+++ wk5/TodoList.sol
@@ -26,43 +26,37 @@ contract TodoList {
   Task[] internal tasks;
 
   // 新增一個 creator address 對 task idx array 的 mapping
-  mapping(address => uint[]) internal tasksByCreator;
+  mapping(uint => address) internal taskToCreator;
+
+  // 定義一個 modifier, 當 msg.sender 是 task creator 時才能呼叫
+  modifier onlyCreator(uint _idx) {
+    require(taskToCreator[_idx] == msg.sender, "Permission Denied.");
+    _;
+  }
 
   // 建立新的 task, 並將它 push 到 tasks 陣列中
   function create(string memory _name) external {
     // 實際產生 task object 並存到 tasks 中
     tasks.push(Task(_name, false));
     // 設定 msg.sender 為 task 的 creator
-    tasksByCreator[msg.sender].push(tasks.length - 1);
+    taskToCreator[tasks.length - 1] = msg.sender;
   }
 
   // 只允許 creator 更新指定 idx 的 task 的 isCompleted 狀態
-  // Note: 使用 for 迴圈會因為 element 增加而消耗越來越多 gas
-  // 50961 -> 53738 -> 56503
-  function update(uint _idx) external returns (bool) {
-    for (uint i = 0; i < tasksByCreator[msg.sender].length; i++) {
-      if (tasksByCreator[msg.sender][i] == _idx) {
-        // 透過 index 找到指定 id 的 task
-        Task storage _task = tasks[_idx];
+  // Note: 使用 for 迴圈時會因為 element 增加而消耗越來越多 gas
+  // 29769 -> 32534 -> 35299
+  // 改寫後： 27181 -> 27181 -> 27181
+  function update(uint _idx) external onlyCreator(_idx) {
+    // 透過 index 找到指定 id 的 task
+    Task storage _task = tasks[_idx];
 
-        // 更新 task 的 isCompleted 狀態
-        _task.isCompleted = !_task.isCompleted;
-
-        // 若有完成更新就直接 return
-        return true;
-      }
-    }
-    revert("Task Not Found.");
+    // 更新 task 的 isCompleted 狀態
+    _task.isCompleted = !_task.isCompleted;
   }
 
   // 只允許 creator 取得指定 idx 的 task detail
-  function get(uint _idx) external view returns (Task memory) {
-    for (uint i = 0; i < tasksByCreator[msg.sender].length; i++) {
-      if (tasksByCreator[msg.sender][i] == _idx) {
-        return tasks[_idx];
-      }
-    }
-    revert("Task Not Found.");
+  function get(uint _idx) external view onlyCreator(_idx) returns (Task memory) {
+    return tasks[_idx];
   }
 
   // 自我毀滅
