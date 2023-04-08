bc47642696a7aec90d4876228be421163a34ef58
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-08 20:48:01 +0800

    update todolist to allow everyone create and get, update their owne tasks
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- wk5/TodoList.sol
+++ wk5/TodoList.sol
@@ -25,24 +25,44 @@ contract TodoList {
   // 後面還會定義 get tasks 的方法, 所以這邊宣告為 internal 即可
   Task[] internal tasks;
 
+  // 新增一個 creator address 對 task idx array 的 mapping
+  mapping(address => uint[]) internal tasksByCreator;
+
   // 建立新的 task, 並將它 push 到 tasks 陣列中
-  function create(string memory _name) external onlyOwner {
+  function create(string memory _name) external {
     // 實際產生 task object 並存到 tasks 中
     tasks.push(Task(_name, false));
+    // 設定 msg.sender 為 task 的 creator
+    tasksByCreator[msg.sender].push(tasks.length - 1);
   }
 
-  // 更新指定 idx 的 task 的 isCompleted 狀態
-  function update(uint _idx) external onlyOwner {
-    // 透過 index 找到指定 id 的 task
-    Task storage _task = tasks[_idx];
+  // 只允許 creator 更新指定 idx 的 task 的 isCompleted 狀態
+  // Note: 使用 for 迴圈會因為 element 增加而消耗越來越多 gas
+  // 50961 -> 53738 -> 56503
+  function update(uint _idx) external returns (bool) {
+    for (uint i = 0; i < tasksByCreator[msg.sender].length; i++) {
+      if (tasksByCreator[msg.sender][i] == _idx) {
+        // 透過 index 找到指定 id 的 task
+        Task storage _task = tasks[_idx];
 
-    // 更新 task 的 isCompleted 狀態
-    _task.isCompleted = !_task.isCompleted;
+        // 更新 task 的 isCompleted 狀態
+        _task.isCompleted = !_task.isCompleted;
+
+        // 若有完成更新就直接 return
+        return true;
+      }
+    }
+    revert("Task Not Found.");
   }
 
-  // 取得指定 idx 的 task detail
-  function get(uint _idx) external view onlyOwner returns (Task memory) {
-    return tasks[_idx];
+  // 只允許 creator 取得指定 idx 的 task detail
+  function get(uint _idx) external view returns (Task memory) {
+    for (uint i = 0; i < tasksByCreator[msg.sender].length; i++) {
+      if (tasksByCreator[msg.sender][i] == _idx) {
+        return tasks[_idx];
+      }
+    }
+    revert("Task Not Found.");
   }
 
   // 自我毀滅
