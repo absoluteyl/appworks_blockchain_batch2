337787c778d67eb1170fd2ecf1ad15a87b34c7db
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-13 14:24:08 +0800

    add Payable contract
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ wk6/Payable.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Payable {
+  address payable public owner;
+
+  constructor() {
+    owner = payable(msg.sender);
+  }
+
+  // A function can only receive Ether if it is marked as payable.
+  function deposit() external payable {}
+
+  function getBalance() external view returns (uint) {
+    return address(this).balance;
+  }
+}
