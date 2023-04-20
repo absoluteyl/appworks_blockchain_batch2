2c3829c579febdfc67c11ad9ed2b7728e79134d5
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-20 22:36:58 +0800

    add sendEther function to my contract
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- wk7/src/MyContract.sol
+++ wk7/src/MyContract.sol
@@ -10,10 +10,19 @@ contract MyContract {
   address public user2;
 
   event Receive(address from, uint256 amount);
+  event Send(address to, uint256 amount);
 
   constructor(address _user1, address _user2) {
     user1 = _user1;
     user2 = _user2;
+  }
+
+  function sendEther(address to, uint256 amount) external payable {
+    require(msg.sender == user1 || msg.sender == user2, "only user1 or user2 can send");
+    require(address(this).balance >= amount, "insufficient balance");
+    (bool success, ) = to.call{ value: amount }("");
+    require(success, "transfer failed");
+    emit Send(to, amount);
   }
 
   receive() external payable {
