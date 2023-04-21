cc00e30faca5ed11d6189f51fc46cb097350a007
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-21 18:08:58 +0800

    use Transfer event instead of Deposit/Withdraw events
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- wk7/test/MyWeth.t.sol
+++ wk7/test/MyWeth.t.sol
@@ -22,7 +22,7 @@ contract MyWethTest is Test {
   }
 
   // Deposit: 當 user1 呼叫 deposit 並發送 ether 時，應：
-  // 1：contract emit Deposit event
+  // 1：contract emit Transfer event
   // 2：contract 將與 msg.value 等值的 ERC20 token mint 給 user1
   // 3：user1 發送的 ether 存入合約
   // 4：user1 帳戶的 ether 減少與 msg.value 相等的值
@@ -30,7 +30,7 @@ contract MyWethTest is Test {
 
   // Withdraw： 當 user1 呼叫 withdraw 時，應：
   // 1: 金額不能超過 user1 的 ERC20 token 餘額
-  // 2: contract emit Withdraw event
+  // 2: contract emit Transfer event
   // 3: user1 的 ERC20 token 餘額減少與 _amount 相等的值
   // 4: 要 burn 掉與 _amount 相等的 ERC20 token (totalSupply 減少)
   // 5: 將與 msg.value 等值的 ether 轉給 user1
@@ -40,7 +40,7 @@ contract MyWethTest is Test {
     vm.startPrank(user1);
     // Deposit
     vm.expectEmit();
-    emit Deposit(address(instance), user1, 0.5 ether);
+    emit Transfer(address(instance), user1, 0.5 * 10**18);
     instance.deposit{value: 0.5 ether}();
     assertEq(instance.balanceOf(user1),  0.5 * 10**18);
     assertEq(address(instance).balance, 0.5 ether);
@@ -51,7 +51,7 @@ contract MyWethTest is Test {
     vm.expectRevert("Insufficient balance");
     instance.withdraw(1 * 10**18);
     vm.expectEmit();
-    emit Withdraw(user1, address(instance), 0.3 ether);
+    emit Transfer(user1, address(instance), 0.3 ether);
     instance.withdraw(0.3 * 10**18);
     assertEq(instance.balanceOf(user1), 0.2 * 10**18);
     assertEq(address(instance).balance, 0.2 ether);
