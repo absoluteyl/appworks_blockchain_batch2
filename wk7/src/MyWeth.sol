be0edbb76e456e1e64bd7f92548b1d486fd8f6e2
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-27 18:49:01 +0800

    mint and burn should use address 0 instead of contract address
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- wk7/src/MyWeth.sol
+++ wk7/src/MyWeth.sol
@@ -83,14 +83,14 @@ contract MyWeth is IWETH9 {
   function mint(uint amount) public {
     balanceOf[msg.sender] += amount;
     totalSupply += amount;
-    emit Transfer(address(this), msg.sender, amount);
+    emit Transfer(address(0), msg.sender, amount);
   }
 
   // Burn some tokens
   function burn(uint amount) public {
     balanceOf[msg.sender] -= amount;
     totalSupply -= amount;
-    emit Transfer(msg.sender, address(this), amount);
+    emit Transfer(msg.sender, address(0), amount);
   }
 
   // ↓↓↓ 課堂練習的部份 ↓↓↓
